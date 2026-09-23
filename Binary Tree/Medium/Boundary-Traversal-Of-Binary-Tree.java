603fcf718f3f41ae9098f569de0889ae9b88e711
Aditya Bhadouria <adiibhadouria@gmail.com>
2026-09-23 15:44:47 +0530

    Add Boundary Traversal Of Binary Tree (Binary Tree/Medium)

---
+++ Binary Tree/Medium/Boundary-Traversal-Of-Binary-Tree.java
@@ -0,0 +1,46 @@
+/*
+ * Boundary Traversal Of Binary Tree
+ * Platform: GeeksforGeeks
+ * Difficulty: Not specified
+ * Topic: Binary Tree > Medium
+ * Time complexity: O(N)
+ * Space complexity: O(H)
+ * Solved: 2026-09-23
+ * URL: https://www.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
+ * Language: Java
+ *
+ * Problem statement:
+ * Not captured.
+ *
+ * Notes:
+ * just broke it down into smaller parts and else is written there , also try its iterative approach
+ */
+
+class Solution {
+    public ArrayList<Integer> boundaryTraversal(Node root) {
+        /* also see the iterative version of it
+        1. Add left boundary iteratively
+        2. Traverse all leaves
+        3. Store right boundary separately
+        4. Add right boundary in reverse
+        first go for left boundary without leaves
+         then leaves
+         then right boundary in reverse*/
+        
+        ArrayList<Integer> boundary = new ArrayList<>();
+        if(root == null) return boundary;
+        if(!isLeaf(root))
+               boundary.add(root.data);
+   
+        leftBoundary(root.left,boundary);
+        leaf(root,boundary);
+        rightBoundary(root.right,boundary);
+        return boundary;
+    }
+    
+    public void leftBoundary(Node node,ArrayList<Integer> boundary){
+        if(node == null || isLeaf(node)) return;
+        /*here we want answer in left boundary so just adding values 
+        as soon as we are getting it now refer right boundary*/
+        boundary.add(node.data);
+        if(node.left!=null){
